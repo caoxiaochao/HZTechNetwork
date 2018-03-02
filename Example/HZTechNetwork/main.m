efd0cd1c7f92364e29594d5bfab944569f73faf3
武汉一青科技有限公司 <ccly080518@163.com>
2018-03-02 15:18:20 +0800

    Initial commit

---
+++ Example/HZTechNetwork/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HZTechNetwork
+//
+//  Created by ccly080518@163.com on 03/02/2018.
+//  Copyright (c) 2018 ccly080518@163.com. All rights reserved.
+//
+
+@import UIKit;
+#import "HZTechAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HZTechAppDelegate class]));
+    }
+}
